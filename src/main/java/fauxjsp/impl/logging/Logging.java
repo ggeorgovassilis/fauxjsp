73f71a2d07952203dea45b2cfe7e4e5904fd8b87
george <george@hiphop>
2015-12-03 14:05:49 +0000

    Setter for other logging implementations

--- src/main/java/fauxjsp/impl/logging/Logging.java
+++ src/main/java/fauxjsp/impl/logging/Logging.java
@@ -5,38 +5,48 @@ import fauxjsp.api.logging.Logger;
 import fauxjsp.impl.logging.java.JavaLoggingFactory;
 
 /**
- * Get a logger instance
+ * Logging. JspServlet will instantiate it by calling setupLogging which sets up
+ * a log4j logger if found in the classpath, otherwise java.util.logging is
+ * used. Other logging implementations can be set with
+ * {@link #setLogFactory(LogFactory)} at any time.
+ * 
  * @author George Georgovassilis
  *
  */
 public class Logging {
 
 	public static LogFactory logFactory;
-	
-	protected static boolean classExists(String fullName){
+
+	protected static boolean classExists(String fullName) {
 		try {
-			return null!=Class.forName(fullName);
+			return null != Class.forName(fullName);
 		} catch (ClassNotFoundException e) {
 			return false;
 		}
 	}
-	
-	protected static LogFactory instantiate(String className){
+
+	protected static LogFactory instantiate(String className) {
 		try {
-			return (LogFactory)(Class.forName(className).newInstance());
+			return (LogFactory) (Class.forName(className).newInstance());
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	protected static void setupLogging(){
-		if (classExists("org.apache.log4j.Logger"))
-			logFactory = instantiate("fauxjsp.impl.logging.log4j.Log4jFactory");
-		else
-		logFactory = new JavaLoggingFactory();
+
+	public static void setLogFactory(LogFactory logFactory) {
+		Logging.logFactory = logFactory;
 	}
-	
-	public static Logger getLogger(Class<?> c){
+
+	public static void setupLogging() {
+		LogFactory factory;
+		if (classExists("org.apache.log4j.Logger"))
+			factory = instantiate("fauxjsp.impl.logging.log4j.Log4jFactory");
+		else
+			factory = new JavaLoggingFactory();
+		setLogFactory(factory);
+	}
+
+	public static Logger getLogger(Class<?> c) {
 		if (logFactory == null)
 			setupLogging();
 		return logFactory.getLogger(c);
