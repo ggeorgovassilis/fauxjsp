922395bf63863a65067c5a6d4a98342bcb56ab02
george georgovassilis <g.georgovassilis@gmail.com>
2017-06-09 00:17:18 +0200

    Test for missing bundle

--- src/test/java/fauxjsp/test/unittests/TestFmt.java
+++ src/test/java/fauxjsp/test/unittests/TestFmt.java
@@ -46,22 +46,35 @@ public class TestFmt extends BaseTest {
 	}
 
 	@Test
-	public void test_fmt_formatdate() throws Exception{
+	public void test_fmt_bundle_not_found() {
+		JspPage page = parser.parseJsp("WEB-INF/jsp/fmt_no_bundle.jsp");
+
+		session.request.setAttribute(JstlFmtMessage.ATTR_RESOURCE_BUNDLE, null);
+
+		try {
+			renderer.render(page, session);
+		} catch (JspRenderException e) {
+			assertTrue(e.getMessage(), e.getMessage().contains("No resource bundle name found"));
+		}
+	}
+
+	@Test
+	public void test_fmt_formatdate() throws Exception {
 		JspPage page = parser.parseJsp("WEB-INF/jsp/fmt_format_date.jsp");
 		request.setLocale(Locale.UK);
 
 		SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-	    isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		isoFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 		Date date = isoFormat.parse("2010-09-23T14:27:18");
 		request.setAttribute("now", date);
 		request.setAttribute(RenderSession.ATTR_TIMEZONE, TimeZone.getTimeZone("UTC"));
-		
+
 		renderer.render(page, session);
 		String text = getPrettyContent(response);
 
-		String expected = "#1: 14:27:18\n" + "#2: 23-Sep-2010\n" + "#3: 23-Sep-2010 14:27:18\n"
-				+ "#4: 23/09/10 14:27\n" + "#5: 23-Sep-2010 14:27:18\n" + "#6: 23 September 2010 14:27:18 UTC\n"
-				+ "#7: 2010-09-23\n" + "#8: 23/09/10 14:27:18 UTC";
+		String expected = "#1: 14:27:18\n" + "#2: 23-Sep-2010\n" + "#3: 23-Sep-2010 14:27:18\n" + "#4: 23/09/10 14:27\n"
+				+ "#5: 23-Sep-2010 14:27:18\n" + "#6: 23 September 2010 14:27:18 UTC\n" + "#7: 2010-09-23\n"
+				+ "#8: 23/09/10 14:27:18 UTC";
 
 		assertEquals(expected, text);
 	}
@@ -92,7 +105,8 @@ public class TestFmt extends BaseTest {
 			renderer.render(page, session);
 			fail("Excpected failure");
 		} catch (JspRenderException e) {
-			assertTrue(e.getMessage(),e.getMessage().contains("'${now}' is a class java.lang.String but I need a java.util.Date"));
+			assertTrue(e.getMessage(),
+					e.getMessage().contains("'${now}' is a class java.lang.String but I need a java.util.Date"));
 		}
 
 	}
@@ -105,16 +119,9 @@ public class TestFmt extends BaseTest {
 		renderer.render(page, session);
 		String text = getPrettyContent(response);
 
-		String expected = "#1 £120,000.23\n" +
-		"#2 000.231\n" +
-		"#3 120000.231\n" +
-		"#4 120000.231\n" +
-		"#5 023%\n" +
-		"#6 12,000,023.1000000000%\n" +
-		"#7 023%\n" +
-		"#8 120E3\n" +
-		"#9 $120,000.23";
-		
+		String expected = "#1 £120,000.23\n" + "#2 000.231\n" + "#3 120000.231\n" + "#4 120000.231\n" + "#5 023%\n"
+				+ "#6 12,000,023.1000000000%\n" + "#7 023%\n" + "#8 120E3\n" + "#9 $120,000.23";
+
 		assertEquals(expected, text);
 	}
 
---
+++ src/test/resources/webapp/WEB-INF/jsp/fmt_no_bundle.jsp
@@ -0,0 +1,2 @@
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+The name of the game is <fmt:message key="game"/>
